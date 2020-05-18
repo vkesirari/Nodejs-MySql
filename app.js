150ea903ebb850980ab9a54a141beaa071b597b7
vikramjeet <uic.17mca8066@gmail.com>
2020-05-18 21:50:30 +0530

    Sql done

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +11,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+// db.execute('SELECT * FROM products')
+//     .then(data => {
+//         console.log(data[0]);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
